9d9272caf6e5a409cb5f89088e7b2fc471a41614
StalinMuela <stalinsangucho87@gmail.com>
2024-06-20 09:56:02 -0500

    Interfaz

--- InterfazGrafica/src/Form1.java
+++ InterfazGrafica/src/Form1.java
@@ -1,5 +1,16 @@
 import javax.swing.*;
 
-public class Form1 {
-    private JButton button1;
+public class Form1 extends JFrame {
+    private JButton ingresaAquiButton;
+
+    public Form1() {
+        setTitle("Form 1");
+        setSize(400, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(null);
+    }
+
+    private void createUIComponents() {
+        System.out.println("Creando un Button");
+    }
 }
--- InterfazGrafica/src/Main.java
+++ InterfazGrafica/src/Main.java
@@ -1,7 +1,14 @@
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
+        String primerNumero = JOptionPane.showInputDialog("Ingrese el primer numero");
+        String segundoNumero = JOptionPane.showInputDialog("Ingrese el segundo numero");
 
-        System.out.println("Hello world!");
+        int numero1 = Integer.parseInt(primerNumero);
+        int numero2 = Integer.parseInt(segundoNumero);
+        int suma = numero1 + numero2;
+        JOptionPane.showMessageDialog(null, "El resultado de la suma es: " + suma,"Suma", JOptionPane.PLAIN_MESSAGE);
     }
 }
