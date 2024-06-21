19be105ead7e5f6f49d090fd618683b9bbf5ba7b
StalinMuela <stalinsangucho87@gmail.com>
2024-06-21 12:49:27 -0500

    dsd

---
+++ ejercicio2/src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("Mi aplicacion");
+        frame.setContentPane(new Form().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(2500, 2000);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
