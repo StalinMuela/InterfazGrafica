337c8153b73f81d5cbee6fc7eb979dabf09610a5
StalinMuela <stalinsangucho87@gmail.com>
2024-07-05 12:44:23 -0500

    buscar

--- insertar/src/Main.java
+++ insertar/src/Main.java
@@ -4,17 +4,18 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
 
-        String url  = "jdbc:mysql://localhost:3306/estudiantesA";
-        String user = "root";
-        String password = "123456";
+            String url  = "jdbc:mysql://localhost:3306/estudiantesA";
+            String user = "root";
+            String password = "123456";
 
-        String nombre = "David Muela";
-        String cedula = "1753175385";
-        Double b1 = 12.12;
-        Double b2 = 14.89;
+            String nombre = "David Muela";
+            String cedula = "17531753851";
+            Double b1 = 12.12;
+            Double b2 = 14.89;
 
-        String sql = "INSERT INTO estudiantes(cedula, nombre, b1, b2) VALUES (?, ?, ?, ?)";
+        String sql = "INSERT INTO estudiantes (cedula, nombre, b1, b2) VALUES (?, ?, ?, ?)";
         try (Connection connection = DriverManager.getConnection(url,user,password)){
+            System.out.println("CONECTADO A LA BASE");
             PreparedStatement cadena = connection.prepareStatement(sql);
             cadena.setString(1,cedula);
             cadena.setString(2,nombre);
